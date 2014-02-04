a4aadabde9210ef5d39a5f264515d21f6772d0f0
Alex Hurd <odty101@gmail.com>
2014-02-04 16:03:02 -0500

    change default thread count to 8

--- start_miner.sh
+++ start_miner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/./minerd -t 4 -a scrypt --url stratum+tcp://wafflepool.com:3333 -O 13TQSuravgc4KuKFrTk3gYdURCncZuVrLW:miner123
+/usr/local/bin/./minerd -t 8 -a scrypt --url stratum+tcp://wafflepool.com:3333 -O 13TQSuravgc4KuKFrTk3gYdURCncZuVrLW:miner123
